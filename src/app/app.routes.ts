88f057f068b078be9b5a308ee0ca5373f2fcf5fa
niveditarrao27 <niveditarao99@gmail.com>
2026-01-06 00:28:03 +0530

    Day 4: Set up routing with lazy-loaded feature modules and basic placeholders

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'auth',
+        loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule),
+    },
+    {
+        path: 'dashboard',
+        canActivate: [authGuard],
+        loadChildren: () => import('./features/dashboard/dashboard.module').then(m => m.DashboardModule),
+    },
+    {
+        path: 'tasks',
+        canActivate: [authGuard],
+        loadChildren: () => import('./features/tasks/tasks.module').then(m => m.TasksModule),
+    },
+    {
+        path: 'users',
+        canActivate: [authGuard],
+        loadChildren: () => import('./features/users/users.module').then(m => m.UsersModule),
+    },
+    {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: 'dashboard'
+    }
+];
--- src/app/features/users/users-routing.module.ts
+++ src/app/features/users/users-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserListComponent } from './containers/user-list/user-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: UserListComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
