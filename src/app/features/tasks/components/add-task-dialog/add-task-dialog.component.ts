9cf35d3767fa2f3f1cdf15d115ab7c9961585149
niveditarrao27 <niveditarao99@gmail.com>
2026-01-15 06:23:02 +0530

    Day 8: Added create task dialog using Angular Material and reactive forms

---
+++ src/app/features/tasks/components/add-task-dialog/add-task-dialog.component.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { MatDialogRef, MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { CommonModule } from '@angular/common';
+import { Task } from '../../models/tasks.model';
+
+@Component({
+  selector: 'app-add-task-dialog',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ],
+  templateUrl: './add-task-dialog.component.html',
+  styleUrl: './add-task-dialog.component.scss'
+})
+export class AddTaskDialogComponent {
+  taskForm!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private dialogRef: MatDialogRef<AddTaskDialogComponent>
+  ) {
+
+    this.taskForm = this.fb.group({
+      title: ['', Validators.required],
+      description: [''],
+      status: ['TODO', Validators.required],
+      priority: ['MEDIUM', Validators.required],
+      dueDate: [new Date(), Validators.required]
+    });
+  }
+
+
+
+  submit(): void {
+    if (this.taskForm.invalid) return;
+
+    const newTask: Task = {
+      id: Date.now(),
+      ...this.taskForm.value
+    } as Task;
+
+    this.dialogRef.close(newTask);
+  }
+}
--- src/app/features/tasks/containers/task-list/task-list.component.ts
+++ src/app/features/tasks/containers/task-list/task-list.component.ts
@@ -6,10 +6,29 @@ import { TasksService } from '../../services/tasks.service';
 import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
 import { MatChipsModule } from '@angular/material/chips';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { AddTaskDialogComponent } from '../../components/add-task-dialog/add-task-dialog.component';
 @Component({
   selector: 'app-task-list',
-  imports: [NavbarComponent, CommonModule, MatTableModule, MatChipsModule],
+  imports: [NavbarComponent, CommonModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatChipsModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.scss'
 })
@@ -18,12 +37,25 @@ export class TaskListComponent {
   tasks: Task[] = [];
   displayedColumns: string[] = ['title', 'status', 'priority', 'dueDate'];
   constructor(
-    private tasksService: TasksService
+    private tasksService: TasksService,
+    private dialog: MatDialog
   ) { }
 
   ngOnInit(): void {
     this.tasksService.getTasks().subscribe((tasks: Task[]) => {
       this.tasks = tasks;
+    });
+  }
+
+  openAddTaskDialog(): void {
+    const dialogRef = this.dialog.open(AddTaskDialogComponent, {
+      width: '400px'
+    });
+
+    dialogRef.afterClosed().subscribe((newTask) => {
+      if (newTask) {
+        this.tasks = [...this.tasks, newTask];
+      }
     });
   }
 
